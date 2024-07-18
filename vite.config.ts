262817e377494045882bba1a6e6a850a6ca25f9b
Otha Folkes <2713193571@qq.com>
2024-07-18 11:15:35 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,127 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+//
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+//
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import { VantResolver } from 'unplugin-vue-components/resolvers'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { terser } from 'rollup-plugin-terser';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { resolve } from 'path';
+import { loadEnv } from 'vite';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import eslintPlugin from 'vite-plugin-eslint';
+import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      dts: false,
-      // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
-      resolvers: [VantResolver({ importStyle: false })]
-    }),
-    createSvgIconsPlugin({
-      // 指定图标文件夹，绝对路径（NODE代码）
-      // eslint-disable-next-line no-undef
-      iconDirs: [path.resolve(process.cwd(), 'src/icons')]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+
+export default ({ mode }: any) => {
+  return defineConfig({
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+        mangle: {
+          reserved: ['console'], // 如果你想保留其他 console 方法，请将它们添加到这个数组中
+        },
+      },
+      target: ['chrome71'],
+      // sourcemap: true, //
+      sourcemap: false, //不要.js.map文件
+      outDir: mode, // 指定输出路径，要和库的包区分开
+      assetsDir: 'static/', // 指定生成静态资源的存放路径
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js1/[name]-[hash].js',
+          entryFileNames: 'static/js2/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        },
+        brotliSize: false, // 不统计
+        target: 'esnext',
+        minify: 'esbuild', // 混淆器，terser构建后文件体积更小
+        presets: [
+          [
+            '@vue/cli-plugin-babel/preset',
+            {
+              useBuiltIns: 'entry',
+            },
+          ],
+        ],
+        plugins: [
+          // "@vue/babel-plugin-jsx",
+          // '@rollup/plugin-babel',
+          terser({
+            compress: {
+              drop_console: true,
+            },
+            mangle: {
+              reserved: ['console'], // 如果你想保留其他 console 方法，请将它们添加到这个数组中
+            },
+          }),
+        ],
+      },
+    },
+
+    plugins: [
+      vue(),
+      vueDevTools({
+        launchEditor: 'webstorm',
+      }),
+      vueJsx(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        imports: ['vue', 'vue-router'],
+      }),
+      eslintPlugin({
+        include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'), // 路径别名
+        VIEWS: resolve(__dirname, 'src/views/layout'), // 路径别名
+      },
+      extensions: ['.js', '.json', '.ts', '.css'], // 使用路径别名时想要省略的后缀名，可以自己 增减
+    },
+    server: {
+      port: 3006,
+      open: false,
+      host: '0.0.0.0',
+      proxy: {
+        '/request': {
+          target: loadEnv(mode, process.cwd()).VITE_API_DOMAIN,
+          changeOrigin: true,
+          rewrite: (path: any) => path.replace(/^\/request/, ''),
+        },
+        '/homes': {
+          target: loadEnv(mode, process.cwd()).VITE_API_HOME,
+          changeOrigin: true,
+          rewrite: (path: any) => path.replace(/^\/homes/, ''),
+        },
+        '/tools': {
+          target: loadEnv(mode, process.cwd()).VITE_API_TOOL,
+          changeOrigin: true,
+          rewrite: (path: any) => path.replace(/^\/tools/, ''),
+        },
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: `@import "./src/assets/";`
+        },
+      },
+    },
+    base: '/',
+  });
+};
